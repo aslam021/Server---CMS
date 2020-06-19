78cda18ed3922a955bc382ad18afb44830f06d06
aslam021 <muhammadh.aslamm@gmail.com>
2020-06-19 21:11:27 +0530

    minor update

--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,15 @@ router.route('/signup')
 .post(cors.corsWithOptions, (req, res, next) => {
 
   const userData = {
-    id: 2,
-    name: req.body.name,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
     email: req.body.email,
     password: req.body.password,
+    // country_code: req.body.country_code
   }
+  if (userData.country_code==null) 
+    userData.country_code = 'AS';
+    
   const query = `SELECT * FROM users WHERE email = '${userData.email}' LIMIT 1`;
 
   db.read(query, req, res, (result)=>{
@@ -28,7 +32,9 @@ router.route('/signup')
     res.json({success: false, token: null, status: userData.email + ' has already registerd!'});
   
   }, () => {
-      const query = `INSERT INTO users VALUES ('${userData.name}','aslam','as','${userData.email}','a','AS','2020-06-11 09:40:37',NULL)`;
+      const query = `INSERT INTO users(first_name, last_name, email, password, country_code) 
+      VALUES ('${userData.first_name}','${userData.last_name}','${userData.email}','${userData.password}','${userData.country_code}')`;
+      
       db.create(query, req, res, (result)=>{
       
       const token = authenticate.getToken({_email: userData.email});
@@ -52,7 +58,7 @@ router.route('/login')
   
   db.read(query , req, res, (user) => {
     console.log(user);
-    if(user.length == 0 || user[0].password === password ){
+    if(user[0].password === password ){
 
       const token = authenticate.getToken({_email: user[0].email});
 
@@ -65,6 +71,10 @@ router.route('/login')
       res.setHeader('Content-Type', 'application/json');
       res.json({success: false, token: null, status: 'Email or password invalid!'});
     }
+  }, ()=>{
+      res.statusCode = 403;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, token: null, status: 'Email or password invalid!'});
   });
 });
 
